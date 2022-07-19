71ff34d5376afd9693e9fb64c725a932dfad9885
Edouard VIGUIER <33686588+V0ri4n@users.noreply.github.com>
2022-07-19 15:26:25 +0200

    FirstCommit

--- src/components/fei-protocol-roles/fei-protocol-roles.js
+++ src/components/fei-protocol-roles/fei-protocol-roles.js
@@ -30,11 +30,14 @@ class MainContent extends Component {
           <div className="card section">
             <h1>Fei Tools</h1>
             <p className="mt-3">
-              This website hosts third-party tools to interact with <a href="https://fei.money/">Fei Protocol</a> and more broadly the Tribe DAO AWESOME products.
+              This website hosts third-party tools to interact with <a href="https://fei.money/">Fei Protocol</a> and more broadly the Tribe DAO products.
             </p>
             <p>These tools are created by TRIBE community members, get in touch with Eswak if you want to suggest changes or add a tool to this website.</p>
             <p className="mb-0">
               <a className="btn" href="#/CollateralizationOracle">Collateralization Oracle</a>
+            </p>
+            <p className="mb-0">
+              <a className="btn" href="#/FeiProtocolRoles">Fei Protocol Roles</a>
             </p>
             <hr/>
             <p className="mb-0">
