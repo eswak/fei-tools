bbc40530ba114d3351a8bd350a3932231e1dbeef
0xVorian <33686588+0xVorian@users.noreply.github.com>
2022-09-01 13:03:45 +0200

    cleaned out dummy theta token data
    cleaned out dummy theta token data and calls

--- src/components/rari-hack-redeem/eligibility.js
+++ src/components/rari-hack-redeem/eligibility.js
@@ -1,7 +1,6 @@
 import { checkProperties } from 'ethers/lib/utils';
 import React, { Component, useState } from 'react';
 import { useAccount, useContractRead } from 'wagmi';
-import redeemerAbi from '../../abi/theta.json'
 import { balances, cTokens } from './balances';
 import Row from './row';
 
@@ -10,38 +9,26 @@ import Row from './row';
 export function RariHackEligibility() {
   const account = useAccount().address
   const [redeemable, setRedeemable] = useState([])
+  const [loaded, setLoaded] = useState(false)
 
 
-  const contractRead = useContractRead({
-    addressOrName: '0x3883f5e181fccaF8410FA61e12b59BAd963fb645',
-    contractInterface: redeemerAbi,
-    functionName: 'balanceOf',
-    args: account
-  })
-
+  // finding out what the user can redeem
   function canRedeem() {
+    if(!loaded){
     for (let i = 0; i < cTokens.length; i++) {
       const instance = {
         cToken: cTokens[i],
         balance: balances[cTokens[i]][account]
       }
-      console.log("instance is")
-      console.log(instance)
       setRedeemable(redeemable => [...redeemable, instance])
-      console.log("redeemable is")
-      console.log(redeemable)
     }
-    console.log(redeemable)
+    setLoaded(true)
   }
-
-
+  }
 
   // render the data
   return (
     <div className="rarihackeligilibity">
-
-      <span>You can withdraw {`${contractRead.data / 1e18}`} tokens</span>
-      <hr />
       <p><button onClick={canRedeem}> Balances </button></p>
       <table className="mb-3">
         <thead>
