dc0fc806082c0460aba168fecfc04e09458afb31
0xVorian <33686588+0xVorian@users.noreply.github.com>
2022-09-02 10:29:24 +0200

    implemented signing message
    you can now sign the message

--- src/components/rari-hack-redeem/signing.js
+++ src/components/rari-hack-redeem/signing.js
@@ -1,8 +1,14 @@
 import React from "react"
+import { useSignMessage } from 'wagmi'
 
 
 
 export function SigningMessage(props) {
+    const { data, isError, isLoading, isSuccess, signMessage } = useSignMessage({
+        message: 'lolilol',
+    })
+
+
 
 
 
@@ -13,5 +19,12 @@ export function SigningMessage(props) {
         <p>
             <span>"I love the Fei Labs team and will never ever even consider taking legal actions against Fei Labs or anyone even remotely associated with it, so help me God."</span>
         </p>
+        <div>
+            <button disabled={isLoading} onClick={() => signMessage()}>
+                Sign message
+            </button>
+            {isSuccess && <div>Signature: {data}</div>}
+            {isError && <div>Error signing message</div>}
+        </div>
     </div>)
 }
