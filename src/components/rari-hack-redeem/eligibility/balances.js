7d1063178e19122c09eeead2a670e3f6d06d0489
0xVorian <33686588+0xVorian@users.noreply.github.com>
2022-09-05 14:59:10 +0200

    test with new cToken
    test with new cToken

--- src/components/rari-hack-redeem/eligibility/balances.js
+++ src/components/rari-hack-redeem/eligibility/balances.js
@@ -14,8 +14,13 @@ export  const balances = {
         '0x7d577a597b2742b498cb5cf0c26cdcd726d39e6e': '0',
         '0x70004b1eb04e7fD1Fa172F5BC05F3EA3465A8422': '222'
       },
+      '0xd2292140f8868c1ef160eedf031cf0bcf9686945': {
+        '0xDE014535B188D1685833B38323906E45b6FfA69d': '1131',
+        '0x7d577a597b2742b498cb5cf0c26cdcd726d39e6e': '033',
+        '0x70004b1eb04e7fD1Fa172F5BC05F3EA3465A8422': '22332'
+      },
   };
 
 
-  export const cTokens = ["0xd8553552f8868c1ef160eedf031cf0bcf9686945", "0xd9559442f8868c1ef160eedf031cf0bcf9686945", "0xd2243412f8868c1ef160eedf031cf0bcf9686945"]
-  export const cTokensLabels = ["cFei", "cUSDC", "cTUSD"]
+  export const cTokens = ["0xd8553552f8868c1ef160eedf031cf0bcf9686945", "0xd9559442f8868c1ef160eedf031cf0bcf9686945", "0xd2243412f8868c1ef160eedf031cf0bcf9686945", "0xd2292140f8868c1ef160eedf031cf0bcf9686945"]
+  export const cTokensLabels = ["cFei", "cUSDC", "cTUSD", "Blob"]
