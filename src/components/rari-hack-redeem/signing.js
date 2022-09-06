c7f24f5a9944a1702040e8c8fe0e848bbaaf7130
0xVorian <33686588+0xVorian@users.noreply.github.com>
2022-09-06 13:48:40 +0200

    cleaned up signing file
    cleaned up signing file, right now configured for signing and submitting signing to contract

--- src/components/rari-hack-redeem/signing.js
+++ src/components/rari-hack-redeem/signing.js
@@ -1,42 +1,57 @@
 import { ethers } from "ethers"
 import React, { useState } from "react"
-import { useProvider, useSignMessage, useSigner } from 'wagmi'
+import { useProvider, useSignMessage, useSigner, usePrepareContractWrite, useContractWrite } from 'wagmi'
 import MultiMerkleRedeemer from "../../abi/MultiMerkleRedeemer.json"
 
 export function SigningMessage(props) {
-    const [writeContract, setWriteContract] = useState(null)
+    const [signer, setSigner] = useState(useSigner())
+    const [provider, setProvider] = useState(useProvider())
     const { data, isError, isLoading, isSuccess, signMessage } = useSignMessage({
         message: 'Sample message, please update.',
         onSettled(data, error) {
             props.liftMessageData(data)
         }
     })
-
-    const provider = useProvider()
-    const signer = useSigner()
-    console.log("signer is",signer)
-
+    ///1. CHECKING FOR SIGNATURE
+    ////Contract address :
     const contractAddress = "0xB22C255250d74B0ADD1bfB936676D2a299BF48Bd"
-
+    //// Contract instance to check for signature:
     const readContract = new ethers.Contract(contractAddress, MultiMerkleRedeemer, provider)
-
+    /// Checking for signature
     const usersDidSign = readContract.userSignatures("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")
 
-    function sign(){
-        setWriteContract = new ethers.Contract(contractAddress, MultiMerkleRedeemer, signer)
-        writeContract.sign(data)
-        console.log("tried to sign")
-    }
 
+
+
+    ///2. SIGNING
+    const { config, error } = usePrepareContractWrite({
+        addressOrName: contractAddress,
+        contractInterface: MultiMerkleRedeemer,
+        functionName: 'sign',
+        args: data
+    })
+    const { signData, signIsLoading, signIsSuccess, write } = useContractWrite(
+        {
+            config,
+            onError(error) {
+                console.log("error", error)
+            },
+            onSettled(data, error) {
+                console.log("settled", data, error)
+            },
+            onSuccess(data) {
+                console.log("success", data)
+            }
+        })
 
     const display = true
 
-    return (<div>{display == false ? 
-    <div>
-      <button onClick={() => console.log(usersDidSign)}>
-                    Test
-                </button>
-    </div> :
+    return (<div>{display == false ?
+        <div>
+            <button onClick={() => console.log(usersDidSign)}>
+                Test
+            </button>
+        </div> :
         <div>
             <p>
                 <span>Please sign the following message:</span>
@@ -52,10 +67,10 @@ export function SigningMessage(props) {
                 </button>
                 {isSuccess && <div>
                     <p>Message signed, please send.</p>
-                    <p><button onClick={() => sign()}>
-                    Test
-                </button></p>
-                    </div>}
+                    <p><button onClick={() => write()}>
+                        Test
+                    </button></p>
+                </div>}
                 {isError && <div>Error signing message</div>}
             </div>
         </div>}
