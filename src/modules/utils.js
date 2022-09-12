aef7d51d70c337a3631a6b98688b39a1047e9b2a
Erwan Beauvois <erwan@allmyt.eu>
2022-09-12 18:12:38 +0200

    Add formatNumber & formatPercent to utils

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -1,7 +1,20 @@
 module.exports = {
+  formatNumber,
+  formatPercent,
   copyToClipboard
 };
 
+// format a number to XX,XXX,XXX
+function formatNumber(n) {
+  return String(Math.floor(n / 1e18)).replace(/(.)(?=(\d{3})+$)/g, '$1,');
+}
+
+// format a [0, 1] number to a %
+function formatPercent(n) {
+  return Math.floor(n * 100) + '%';
+}
+
+// Copy some string into the clipboard
 function copyToClipboard(arg) {
   var str = arg;
   if (typeof str === 'object') {
